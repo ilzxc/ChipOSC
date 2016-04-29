f42e811ae088136f9c6ef0ab64b2aa87bb114b77
Ilya Rostovtsev <ilyaforpresident@gmail.com>
2016-04-29 00:13:13 -0700

    up

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -18,7 +18,8 @@ int main (int argc, char* argv[])
     OSCSender sender;
     sender.connect( "192.168.2.10", 56765 );
     OSCBundle bundle;
-    bundle.addElement( OSCMessage( "/fromChip", "Hello" ) );
+    auto message = OSCMessage( "/fromChip", String( "hello!" ) );
+    bundle.addElement( message );
     sender.send( bundle );
 
     return 0;
